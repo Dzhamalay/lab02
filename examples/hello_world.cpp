f23f377cbc202130236e2c764fd962bf8025f9fd
Dzhamalay <dzamaevmansur@gmail.com>
2024-03-23 23:10:39 +0300

    was added code that prints 'Hello World'

---
+++ examples/hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+        cout << "Enter your name: ";
+        string name;
+        cin >> name;
+        cout >> "Hello World from " << name << endl;
+        return 0;
+}

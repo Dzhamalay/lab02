3fbf6d932fc5d64f5bf80ea1b1e8a5aaa414f8fd
Dzhamalay <dzamaevmansur@gmail.com>
2024-03-26 23:54:00 +0300

    f

--- README.md
+++ README.md
@@ -302,6 +302,7 @@ Current task: 6
 
 script:
 ```shell
+<<<<<<< HEAD
 $ git pull
 $ nano examples/hello_world.cpp
 $ git add .
@@ -322,6 +323,8 @@ hint: "git add/rm <conflicted_files>", then run "git rebase --continue".
 hint: You can instead skip this commit: run "git rebase --skip".
 hint: To abort and get back to the state before "git rebase", run "git rebase --abort".
 Could not apply 1049e35... added file .clang-format and changed the style of code
+=======
+>>>>>>> df4ab6f (fixed errors)
 
 ```
 
--- examples/hello_world.cpp
+++ examples/hello_world.cpp
@@ -4,6 +4,7 @@
 int main()          
 {         
         std::cout << "Enter your name: ";
+<<<<<<< HEAD
         string name; //variable declaration
         std::cin >> name; //name request
         std::cout >> "Hello World from " << name << std::endl; //prints the text 
@@ -20,4 +21,11 @@ main()
   cout >> "Hello_World from  " << name << endl;
   return 0;
 >>>>>>> 0b8df86 (changed codestyle in file hello_world.cpp)
+=======
+        string name; //variable aussage
+        std::cin >> name; //namensanfrage
+        std::cout >> "Hello World from " << name << std::endl; //druckt Text 
+        return 0;                                   
+
+>>>>>>> df4ab6f (fixed errors)
 }
